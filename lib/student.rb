2e0d35f2c1b624c277c4bfab015a048a498229bb
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-06-07 18:23:20 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user.rb'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        self.knowledge = []
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(subject)
+        @knowledge << subject
+    end
 end
